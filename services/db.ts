0713fcf0e4e4c0d231aa29162845a1c2be4fa7c1
pamidumihiranga12-blip <pamidumihiranga12@gmail.com>
2026-05-24 17:55:20 +0530

    Update db.ts

--- services/db.ts
+++ services/db.ts
@@ -1,21 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-// ඔබේ Firebase Config දත්ත (රූපයේ තිබූ පරිදි)
 const firebaseConfig = {
-  apiKey: "AIzaSyBglGLuQFog9uJ1BF-4aKR0WEIZHPS046M",
+  apiKey: "AIzaSyBglG1uQFog9uJ1BF-4aKR0WEfZHPS046M",
   authDomain: "myweb-7ecb4.firebaseapp.com",
   projectId: "myweb-7ecb4",
   storageBucket: "myweb-7ecb4.firebasestorage.app",
   messagingSenderId: "1027238820127",
   appId: "1:1027238820127:web:7b3d49b74d8161ec53582a",
-  measurementId: "G-81JY5FKV0E"
+  measurementId: "G-81JY5FKVQE"
 };
-
-// Firebase Initialize කිරීම
-const app = initializeApp(firebaseConfig);
-
-// අපිට අවශ්‍ය සේවාවන් Export කිරීම
-export const db = getFirestore(app);
-export const auth = getAuth(app);
